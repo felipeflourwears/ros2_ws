16bacc3cee13c627f8eacc62780577c98486eecd
felipeflourwears <felipeflourwears619@gmail.com>
2025-08-14 14:08:42 -0600

    OOP C++

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,11 +1,28 @@
 #include "rclcpp/rclcpp.hpp"
+using namespace std::chrono_literals;
 
+class MyNode : public rclcpp::Node
+{
+    public:
+        MyNode() : Node("cpp_hello_world_node"), counter_(0)
+        {
+            RCLCPP_INFO(this->get_logger(), "Hello world");
+            timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&MyNode::timerCallback, this));
+        }
+    private:
+        void timerCallback()
+        {
+            RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+            counter_++;
+        } 
+        rclcpp::TimerBase::SharedPtr timer_;
+        int counter_;
+};
 
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
     //Create a pointer Object RCL CPP
-    auto node = std::make_shared<rclcpp::Node>("cpp_hello_world_node");
-    RCLCPP_INFO(node->get_logger(), "Hello world");
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
